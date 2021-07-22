0bed58838ed68aa83457079266d1f26ceed182ab
dfsek <dfsek@protonmail.com>
2021-07-22 11:23:28 -0700

    Create publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,3 +94,28 @@ class Version(
 
 val gitHash
     get() = grgit.head().id.substring(0..7)
+
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["jar"])
+        }
+    }
+    
+    repositories {
+        val mavenUrl = "https://repo.codemc.io/repository/maven-releases/"
+        
+        maven(mavenUrl) {
+            val mavenUsername: String? by project
+            val mavenPassword: String? by project
+            if (mavenUsername != null && mavenPassword != null) {
+                credentials {
+                    username = mavenUsername
+                    password = mavenPassword
+                }
+            }
+        }
+    }
+}
