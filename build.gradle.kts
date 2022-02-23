9bfcafdea70222efa77cc52792d622aaf595464b
solonovamax <solonovamax@12oclockpoint.com>
2022-02-23 13:19:03 -0500

    Add dokka to kotlin extensions to produce documentation jar
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
  * Copyright (c) 2021-2022 solonovamax <solonovamax@12oclockpoint.com>
  *
  * The file build.gradle.kts is part of Strata
- * Last modified on 23-02-2022 12:48 p.m.
+ * Last modified on 23-02-2022 01:00 p.m.
  *
  * MIT License
  *
@@ -44,7 +44,7 @@ subprojects {
     apply(plugin = "org.ajoberstar.grgit")
     
     group = "ca.solo-studios"
-    version = Version("1", "2", "1")
+    version = Version("1", "2", "2")
     
     repositories {
         mavenCentral()
--- strata-kotlin/build.gradle.kts
+++ strata-kotlin/build.gradle.kts
@@ -3,6 +3,37 @@
  * Copyright (c) 2021-2022 solonovamax <solonovamax@12oclockpoint.com>
  *
  * The file build.gradle.kts is part of Strata
+ * Last modified on 23-02-2022 01:19 p.m.
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * STRATA IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
+
+/*
+ * Strata - A library for parsing and comparing version strings
+ * Copyright (c) 2021-2022 solonovamax <solonovamax@12oclockpoint.com>
+ *
+ * The file build.gradle.kts is part of Strata
  * Last modified on 23-02-2022 12:31 p.m.
  *
  * MIT License
@@ -28,6 +59,7 @@
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.dokka") version "1.6.10"
 }
 
 repositories {
@@ -42,4 +74,26 @@ dependencies {
     api(project(":strata"))
     
     testImplementation(kotlin("test"))
+}
+
+val dokkaHtml by tasks.getting(DokkaTask::class)
+
+val javadocJar by tasks.getting(Jar::class) {
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+tasks.withType<DokkaTask> {
+    dokkaSourceSets {
+        named("main") {
+            moduleName.set("Strata Kotlin Extensions")
+            
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URL("https://github.com/solo-studios/Strata/tree/master/strata-kotlin/src/main/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
 }
